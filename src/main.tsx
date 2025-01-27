644d7bb2624fcfbb833b0082c49a59dce86f9d56
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-01-27 15:53:42 +0300

    feat: main font Montserrat was added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App/App.tsx'
+import './styles/reset.css';
+import './styles/variables.css';
+import './assets/fonts/stylesheet.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
