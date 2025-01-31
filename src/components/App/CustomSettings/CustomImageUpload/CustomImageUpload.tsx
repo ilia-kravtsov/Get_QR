dc05a3538da7f9a9a983648b354a3b93c6f97e8b
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-01-31 14:37:17 +0300

    fix: input validation fix

--- src/components/App/CustomSettings/CustomImageUpload/CustomImageUpload.tsx
+++ src/components/App/CustomSettings/CustomImageUpload/CustomImageUpload.tsx
@@ -1,11 +1,10 @@
 import { ChangeEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setUserImage } from '../../../../store/slices/qrSlice.ts'
-import { toast } from 'react-toastify'
 import s from './CustomImageUpload.module.scss'
 import { Button } from '../../common/Button/Button.tsx'
-import { toastConfig } from '../../../../utils/constants.ts'
 import { useTranslation } from '../../../../utils/customHooks.ts'
+import { validateImageUrl } from '../../../../utils/validators.ts'
 
 type Props = {
   handleImageUploadCB: () => void
@@ -13,85 +12,31 @@ type Props = {
 
 export const CustomImageUpload = ({ handleImageUploadCB }: Props) => {
   const dispatch = useDispatch()
-  const [imageUrl, setImageUrl] = useState<string>('')
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [isValidImage, setIsValidImage] = useState<boolean>(false)
-  const [error, setError] = useState<boolean>(false)
   const { t } = useTranslation()
 
-  const isDirectImageLink = (url: string): boolean => {
-    return /\.(jpeg|jpg|png|gif|webp|bmp|svg)$/i.test(url)
-  }
+  const [imageUrl, setImageUrl] = useState<string>('')
+  const [isValidImage, setIsValidImage] = useState<boolean>(false)
 
-  const checkImageMimeType = async (url: string): Promise<boolean> => {
-    try {
-      const response = await fetch(url, { method: 'HEAD' })
-      const contentType = response.headers.get('Content-Type')
-      return contentType?.startsWith('image/') || false
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(`Ошибка при проверке MIME-типа изображения: ${error.message}`)
-        toast.error('Ссылка не ведёт на прямую к файлу с изображением', toastConfig)
-        setError(true)
-      } else {
-        toast.error('Неизвестная ошибка при проверке MIME-типа изображения', toastConfig)
-        setError(true)
-      }
-      return false
+  const handleUrlChange = async (e: ChangeEvent<HTMLInputElement>) => setImageUrl(e.target.value.trim())
+
+  const handleBlurValidation = async () => {
+    const isValid = await validateImageUrl(imageUrl, setIsValidImage)
+    if (isValid) {
+      setIsValidImage(true)
     }
-  }
-
-  const handleUrlChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const url = e.target.value.trim()
-    const urlMatch = url.match(/https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(?:\/[^\s]*)?/)
-    const validUrl = urlMatch ? urlMatch[0] : ''
-
-    setImageUrl(validUrl)
-
-    if (!validUrl || !isDirectImageLink(validUrl)) {
-      toast.info('Введите корректную ссылку ;)', toastConfig)
-      setError(true)
-      setIsValidImage(false)
-      return
-    }
-
-    const isValid = await checkImageMimeType(validUrl)
-    setIsValidImage(isValid)
-    setError(!isValid)
   }
 
   const handleImageUpload = async () => {
-    if (error) {
-      toast.info('Введите корретную ссылку ;)', toastConfig)
-      return
+    const isValid = await validateImageUrl(imageUrl, setIsValidImage)
+    if (isValid) {
+      setIsValidImage(true)
+      dispatch(setUserImage(imageUrl))
+      handleImageUploadCB()
+      setImageUrl('')
     }
-
-    if (!imageUrl) {
-      toast.info('Изображение не загружено', toastConfig)
-      setError(true)
-      return
-    }
-
-    setIsLoading(true)
-
-    if (!isValidImage) {
-      toast.info('Ссылка не является прямой ссылкой на изображение', toastConfig)
-      setIsLoading(false)
-      setError(true)
-      return
-    }
-
-    dispatch(setUserImage(imageUrl))
-    toast.info('Лого добавилось на QR', toastConfig)
-    toast.info('Для отображения лого .gif может понадобиться больше времени', toastConfig)
-    handleImageUploadCB()
-    setImageUrl('')
-    setIsValidImage(false)
-    setIsLoading(false)
-    setError(false)
   }
 
-  const handleImageLoad = () => setIsLoading(false)
+  const handleImageLoadError = () => console.error('Ошибка загрузки лого')
 
   return (
     <div className={s.container}>
@@ -103,6 +48,7 @@ export const CustomImageUpload = ({ handleImageUploadCB }: Props) => {
           id="url-input"
           placeholder={t('placeholderInputImageToQr')}
           onChange={handleUrlChange}
+          onBlur={handleBlurValidation}
           className={s.inputUrl}
           value={imageUrl}
           maxLength={2084}
@@ -116,13 +62,12 @@ export const CustomImageUpload = ({ handleImageUploadCB }: Props) => {
               <p>{t('figcaptionFirstP')}</p>
               <p>{t('figcaptionSecondP')}</p>
             </figcaption>
-            {isLoading && <div className={s.spinner}></div>}
-            <img src={imageUrl} alt="" className={s.imageUrl} onLoad={handleImageLoad} />
+            <img src={imageUrl} alt="" className={s.imageUrl} onError={handleImageLoadError} />
           </figure>
         )}
 
         {imageUrl && isValidImage && (
-          <Button title={t('buttonTitleAddLogoToQR')} onClickCB={handleImageUpload} disabled={error} />
+          <Button title={t('buttonTitleAddLogoToQR')} onClickCB={handleImageUpload} disabled={!isValidImage} />
         )}
       </div>
     </div>
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,4 +1,62 @@
+import { toast } from 'react-toastify'
+import { toastConfig } from './constants.ts'
+
 export const isValidURL = (url: string): boolean => {
   const regex = /^(https?:\/\/)?([\w-]+\.)+[a-zA-Z]{2,}(\/[\w-./?%&=]*)?$/i
   return regex.test(url) && url === url.trim()
 }
+
+export const isDirectImageLink = (url: string): boolean => {
+  return /\.(jpeg|jpg|png|gif|webp|bmp|svg)$/i.test(url)
+}
+
+export const checkImageMimeType = async (url: string, setIsValidImageCB: (value: boolean) => void): Promise<boolean> => {
+  try {
+    const response = await fetch(url, { method: 'HEAD' })
+    const contentType = response.headers.get('Content-Type')
+    return contentType?.startsWith('image/') || false
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Ошибка при проверке MIME-типа изображения: ${error.message}`)
+      toast.error('Ссылка не ведёт на прямую к файлу с изображением', toastConfig)
+      setIsValidImageCB(false)
+    } else {
+      toast.error('Неизвестная ошибка при проверке MIME-типа изображения', toastConfig)
+      setIsValidImageCB(false)
+    }
+    return false
+  }
+}
+
+export const validateImageUrl = async (url: string, setIsValidImageCB: (value: boolean) => void) => {
+  if (!url) {
+    toast.info('Введите корректную ссылку ;)', toastConfig);
+    setIsValidImageCB(false);
+    return false;
+  }
+
+  if (!isValidURL(url)) {
+    toast.error('Некорректная ссылка', toastConfig);
+    setIsValidImageCB(false);
+    return false;
+  }
+
+  if (!isDirectImageLink(url)) {
+    toast.info('Ссылка не является прямой ссылкой на изображение', toastConfig);
+    setIsValidImageCB(false);
+    return false;
+  }
+
+  if (url.toLowerCase().endsWith('.gif')) {
+    toast.info('Для отображения лого .gif может понадобиться больше времени', toastConfig);
+  }
+
+  const isMimeTypeValid = await checkImageMimeType(url, setIsValidImageCB);
+  if (!isMimeTypeValid) {
+    toast.info('Ссылка не является прямой ссылкой на изображение', toastConfig);
+    setIsValidImageCB(false);
+    return false;
+  }
+
+  return true;
+};
